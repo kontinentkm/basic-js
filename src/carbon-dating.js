30982426750aed334394b937a4ff62523c1ccd95
Max <KontinentKM@gmail.com>
2023-04-17 18:16:24 +0300

    feat: add some more tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+	if (typeof sampleActivity !== 'string' || isNaN(sampleActivity)) {
+		return false;
+	 }
+  
+	 const activity = parseFloat(sampleActivity);
+	 if (activity <= 0 || activity >= MODERN_ACTIVITY) {
+		return false;
+	 }
+  
+	 const lambda = 0.693 / HALF_LIFE_PERIOD;
+	 const age = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / lambda);
+  
+	 if(isNaN(age)) return false;
+
+	 return age;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	str = String(str);
+ 
+	let repeatTimes = options.repeatTimes;
+	let separator = options.separator !== undefined ? options.separator : '+';
+	let addition = options.addition !== undefined ? String(options.addition) : '';
+	let additionRepeatTimes = options.additionRepeatTimes;
+	let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+ 
+	let additionString = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+	let result = (str + additionString + separator).repeat(repeatTimes - 1) + str + additionString;
+ 
+	return result;
 }
 
 module.exports = {
   repeater
 };
+
+
